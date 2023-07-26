db52ef10c4d6e1c8ac9e17c08d8a299d9c994876
CodeAGenie <nwibeg@gmail.com>
2023-07-26 11:34:41 +0100

    update

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // Define UI Variables
 const form = document.querySelector('#task-form');
-const tasklist = document.querySelector('.collection');
+const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
-const filter = document-querySelector('#filter');
+const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
 // Load all event listeners
@@ -13,19 +13,28 @@ loadEventlisteners();
 function loadEventlisteners(){
    form.addEventListener('submit', addTask)
 }
+
 function addTask(e){
-    if(taskInput.value===''){
-        alert ('Add a task')
+    if(taskInput.value ===''){
+        alert ('Add a task'); 
     }
     // create li element
     const li = document.createElement('li')
-    // add class
+    // add class  
     li.className = 'collection-item'
     // create text node and append to li
-    li.appendChild(document.createTextNode(taskInput.value));
+    li.appendChild(document.createTextNode(taskInput.value)); 
     // Create new link element
     const link = document.createElement('a')
     // ad class to link
-    link.className = 'delete-item'
+    link.className = 'delete-item  secondary-content';
+    // add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>'
+    // append the link to the list
+    li.appendChild(link)
+    // append li to ul
+    taskList.appendChild(li)
+    // clear input
+    taskInput.value = '';
     e.preventDefault();
 }
