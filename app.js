89c53f351490b9ddc2a5690fce1bb288e92ab4ae
CodeAGenie <nwibeg@gmail.com>
2023-07-26 11:01:12 +0100

    adding functionalities

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 // Define UI Variables
+const form = document.querySelector('#task-form');
+const tasklist = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document-querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+
+loadEventlisteners();
+ 
+// Load all event listeners 
+function loadEventlisteners(){
+   form.addEventListener('submit', addTask)
+}
+function addTask(e){
+    if(taskInput.value===''){
+        alert ('Add a task')
+    }
+    // create li element
+    const li = document.createElement('li')
+    // add class
+    li.className = 'collection-item'
+    // create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create new link element
+    const link = document.createElement('a')
+    // ad class to link
+    link.className = 'delete-item'
+    e.preventDefault();
+}
